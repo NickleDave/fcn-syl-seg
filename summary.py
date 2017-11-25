90f66cc716d5564dc297ba70720b31ada2ff062c
David Nicholson <nicholdav@gmail.com>
2017-11-25 15:55:15 -0500

    summary.py runs all the way through

--- summary.py
+++ summary.py
@@ -3,6 +3,7 @@ import os
 import pickle
 from glob import glob
 from configparser import ConfigParser
+from datetime import datetime
 
 import numpy as np
 from sklearn.externals import joblib
@@ -25,6 +26,11 @@ results_dirname = config['OUTPUT']['output_dir']
 if not os.path.isdir(results_dirname):
     raise FileNotFoundError('{} directory is not found.'
                             .format(results_dirname))
+
+timenow = datetime.now().strftime('%y%m%d_%H%M%S')
+summary_dirname = os.path.join(results_dirname,
+                               'summary_' + timenow)
+os.makedirs(summary_dirname)
 
 TRAIN_SET_DURS = [int(element)
                   for element in
@@ -50,9 +56,6 @@ for spect_param_name in ['freq_cutoffs', 'thresh']:
         continue
 if spect_params == {}:
     spect_params = None
-
-train_err_arr = np.empty((len(TRAIN_SET_DURS), len(REPLICATES)))
-test_err_arr = np.empty((len(TRAIN_SET_DURS), len(REPLICATES)))
 
 print('loading training data')
 labelset = list(config['DATA']['labelset'])
@@ -82,8 +85,7 @@ return_tup = fcn.utils.load_data(label_mapping,
 num_train_songs = int(config['DATA']['num_train_songs'])
 train_spects = song_spects[:num_train_songs]
 X_train = np.concatenate(train_spects, axis=1)
-Y_train = masks[:, :X_train.shape[-1], :]
-assert Y_train.shape[-1] == len(label_mapping) + 1
+Y_train = np.concatenate(all_labeled_timebin_vectors).ravel()
 
 print('loading testing data')
 test_data_dir = config['DATA']['test_data_dir']
@@ -105,11 +107,13 @@ with open(cbins_used_filename, 'wb') as cbins_used_file:
 # X_test_copy because X_test gets scaled and reshaped in main loop
 X_test_copy = np.concatenate(test_song_spects, axis=1)
 # make Y_test here because it doesn't change
+Y_test = np.concatenate(test_labeled_timebin_vectors).ravel()
+
 fcn_width = int(config['TRAIN']['fcn_width'])
 inds_to_split_Y_test = np.arange(start=fcn_width,
                                  stop=test_masks.shape[1],
                                  step=fcn_width)
-Y_test = np.stack(np.split(test_masks, inds_to_split_Y_test, axis=1)[:-1])
+Y_test = np.stack(np.split(Y_test, inds_to_split_Y_test)[:-1])
 
 normalize_spectrograms = config.getboolean('DATA', 'normalize_spectrograms')
 
@@ -119,8 +123,18 @@ TRAIN_SET_DURS = [int(element)
 num_replicates = int(config['TRAIN']['replicates'])
 REPLICATES = range(num_replicates)
 
+# initialize arrays to hold summary results
+train_err_arr = np.empty((len(TRAIN_SET_DURS), len(REPLICATES)))
+test_err_arr = np.empty((len(TRAIN_SET_DURS), len(REPLICATES)))
+Y_pred_test_all = []  # will be a nested list
+Y_pred_train_all = [] # will be a nested list
+
 
 for dur_ind, train_set_dur in enumerate(TRAIN_SET_DURS):
+
+    Y_pred_test_this_dur = []
+    Y_pred_train_this_dur = []
+
     for rep_ind, replicate in enumerate(REPLICATES):
         print("getting train and test error for "
               "training set with duration of {} seconds, "
@@ -136,7 +150,7 @@ for dur_ind, train_set_dur in enumerate(TRAIN_SET_DURS):
 
         # get training set
         X_train_subset = X_train[:, train_inds]
-        Y_train_subset = Y_train[:, train_inds, :]
+        Y_train_subset = Y_train[train_inds]
         # normalize before reshaping to avoid even more convoluted array reshaping
         if normalize_spectrograms:
             print('normalizing spectrograms')
@@ -159,7 +173,7 @@ for dur_ind, train_set_dur in enumerate(TRAIN_SET_DURS):
                       step=fcn_width)
         X_train_subset = np.stack(np.split(X_train_subset, n, axis=1))
         X_train_subset = X_train_subset[:, :, :, np.newaxis]
-        Y_train_subset = np.stack(np.split(Y_train_subset, n, axis=1))
+        Y_train_subset = np.stack(np.split(Y_train_subset, n))
         # below don't keep last array because it might be less wide than fcn_width
         # since test set is not guaranteed to be of length
         # evenly divisible by fcn_width
@@ -168,9 +182,8 @@ for dur_ind, train_set_dur in enumerate(TRAIN_SET_DURS):
                                  step=fcn_width)
         # sanity check
         assert np.array_equal(inds_to_split_X_test, inds_to_split_Y_test)
-        X_test = np.stack(np.split(X_test_copy, inds_to_split_X_test, axis=1)[:-1])
+        X_test = np.stack(np.split(X_test, inds_to_split_X_test, axis=1)[:-1])
         X_test = X_test[:, :, :, np.newaxis]
-
 
         model_filename = os.path.join(training_records_dir,
                                       'fcn_model.h5')
@@ -181,16 +194,40 @@ for dur_ind, train_set_dur in enumerate(TRAIN_SET_DURS):
         outputs2 = Activation('softmax')(outputs2)
         fcn_custom2 = Model(inputs=fcn.input, outputs=outputs2)
         # optimizer='SGD' is not conservative enough, it can fail during training
-        import pdb;pdb.set_trace()
         fcn_custom2.compile(optimizer='rmsprop',
                             loss='categorical_crossentropy',
                             metrics=['accuracy'])
+        Y_pred_train = fcn_custom2.predict(X_train_subset)
+        Y_pred_test = fcn_custom2.predict(X_test)
 
-        y_pred_train = []
-        for ind in range(X_train_subset.shape[0]):
-            y_pred.append(fcn_custom2.predict(spects_split[ind:ind + 1, :, :, :]))
+        Y_pred_train_max = np.argmax(Y_pred_train, axis=2)
+        Y_pred_test_max = np.argmax(Y_pred_test, axis=2)
 
-        import pdb;pdb.set_trace()
+        train_err = np.sum(Y_pred_train_max.ravel()
+                           - Y_train_subset.ravel() != 0) / Y_train_subset.ravel().shape[-1]
+        train_err_arr[dur_ind, rep_ind] = train_err
+        print('train error was {}'.format(train_err))
+
+        test_err = np.sum(Y_pred_test_max.ravel()
+                          - Y_test.ravel() != 0) / Y_test.ravel().shape[-1]
+        test_err_arr[dur_ind, rep_ind] = test_err
+        print('test error was {}'.format(test_err))
+
+        Y_pred_train_this_dur.append(Y_pred_train)
+        Y_pred_test_this_dur.append(Y_pred_test)
+
+    Y_pred_train_all.append(Y_pred_train_this_dur)
+    Y_pred_test_all.append(Y_pred_test_this_dur)
+
+Y_pred_train_filename = os.path.join(training_records_dir,
+                                  'Y_pred_train')
+with open(Y_pred_train_filename,'wb') as Y_pred_train_file:
+    pickle.dump(Y_pred_train_all, Y_pred_train_file)
+
+Y_pred_test_filename = os.path.join(training_records_dir,
+                                  'Y_pred_test')
+with open(Y_pred_test_filename,'wb') as Y_pred_test_file:
+    pickle.dump(Y_pred_test_all, Y_pred_test_file)
 
 train_err_filename = os.path.join(training_records_dir,
                                   'train_err')
